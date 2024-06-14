90bbad278f2e00f4b0717e52b80d3e91f33d44aa
michiru <michiru.shigemitsu@gmail.com>
2024-06-14 22:43:23 +0900

    first commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,26 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @order.ordered_lists.build
+    @order.ordered_lists.build #多対多のモデル。
     @items = Item.all.order(:created_at)
   end
 
   def create
+    
+    #Itemの中で注文されたItemIDの商品をロックしたい
+    #注文されたItemをすべて持ってきて数量が0ではないものだけをロックしたい
     @order = current_user.orders.build(order_params)
-    @order.save
+    #  selected_items = @order.ordered_lists.filter_map {|v| v.item_id if v.quantity > 0}
+    #  selected_items.each do |id|
+    #   item = Item.find(id)
+    #   item.with_lock do
+    #   end
+    #binding.irb
+    @order.with_lock do
+    @order.save!
     @order.update_total_quantity
     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
+    end
     redirect_to orders_path
   end
 
